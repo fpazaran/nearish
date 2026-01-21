af6a91ae4215baa326d19039433ed4ed8ff8a950
fpazaran <138263417+fpazaran@users.noreply.github.com>
2026-01-21 00:13:35 -0800

    AWS Setup
    * Completed LandingPage, Created SquarRounded
    
    * setup AWS RDS postgres db

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
