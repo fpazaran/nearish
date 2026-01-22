995971e7970be39632ea53ed1c76b5487e61e51f
fpazaran <138263417+fpazaran@users.noreply.github.com>
2026-01-22 01:26:25 -0800

    Fix default value for created_at column in user schema

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -7,11 +7,11 @@ class User(Base):
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
     name = Column(String, nullable=False)
     couple_id = Column(Integer, nullable=False, unique=True, foreign_key="couples.id")
-    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
 
 class Couple(Base):
     __tablename__ = "couples"
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
     partner1_uid = Column(Integer, nullable=False, foreign_key="users.id")
     partner2_uid = Column(Integer, nullable=True, foreign_key="users.id")
-    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
