4f27b01bab51092c30c27411f71719f28f9b2bec
fpazaran <fpazarann@gmail.com>
2026-01-25 22:34:33 -0800

    Implemented enter-name page

--- frontend/src/pages/connect-flow/EnterNamePage.tsx
+++ frontend/src/pages/connect-flow/EnterNamePage.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RoundedShadowBox from '../../components/containers/RoundedShadowBox'
 import Background from '../../components/Background'
 import Loading from '../Loading'
 import { useUser } from '../../contexts/UserContext';
+import { USER_NAME_MAX_LENGTH } from '../../api/constants/User';
 
 function EnterNamePage() {
   const user = useUser();
+  const [name, setName] = useState('');
   if (user.loading) {
     return <Loading />
+  }
+
+  const handleContinue = () => {
+    if (name.length > 0) {
+      const onSaveFailed = () => {
+        alert('Failed to save name. Please try again.');
+      }
+      user.updateName(name, onSaveFailed);
+    } else {
+      alert('Please enter your name.');
+    }
   }
 
   return (
@@ -18,10 +31,31 @@ function EnterNamePage() {
             nearish
           </div>
           <RoundedShadowBox>
-            <div className="flex flex-row items-start justify-start">
-              <div className="text-3xl font-bold text-[var(--darker_pink)]">
+            <div className="flex flex-col items-start justify-start w-full gap-5">
+            <div className="flex flex-col items-start justify-start w-full gap-1">
+              <div className="text-3xl font-bold text-[var(--darker_pink)] text-left">
                 What is your name?
               </div>
+              <div className="text-xl font-bold text-[var(--medium_pink)] text-left">
+                This is how your partner will see you.
+              </div>
+            </div>
+            <input
+              type="text"
+              className="w-full rounded-xl px-4 py-3 bg-[var(--dark-medium-pink)] focus:ring-3 focus:ring-[var(--darker_pink)] transition-all duration-400
+              outline-none transition-shadow text-xl font-bold text-[var(--darker_pink)]
+              min-h-12"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter your name"
+              maxLength={USER_NAME_MAX_LENGTH}
+            />
+            <button className="w-full rounded-xl py-3 px-2 bg-[var(--darker_pink)] text-[var(--lightest_pink)] text-2xl 
+                              font-medium text-center flex items-center justify-center transition-opacity duration-100 
+                              hover:opacity-80 active:opacity-20 active:[transition-duration:300ms]"
+                              onClick={handleContinue}>
+              Continue →
+            </button>
             </div>
           </RoundedShadowBox>
         </div>
