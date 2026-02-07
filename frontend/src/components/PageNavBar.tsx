2abf67979d09a32553f0897599c0a17e68f2a2ea
Fernando Pazaran <fpazarann@gmail.com>
2026-02-06 22:50:48 -0800

    Max width of PageNavBar smaller

--- frontend/src/components/PageNavBar.tsx
+++ frontend/src/components/PageNavBar.tsx
@@ -8,7 +8,7 @@ function PageNavBar() {
   }
   return (
     <div className='w-full h-16 absolute top-0 left-0 pt-4 px-6 flex flex-row items-center justify-center gap-4'>
-      <div className="flex-1 flex flex-row items-center justify-start gap-4 max-w-300">
+      <div className="flex-1 flex flex-row items-center justify-start gap-4 max-w-200">
         <IoMdArrowRoundBack className='text-4xl text-[var(--darker_pink)] transition-opacity duration-300 ease-in-out active:opacity-20'
         onClick={handleBack}
         />
